b30bce075d39b0d5897fdc5739e5b78708844c00
lhopkins <lukehopkinsdev@gmail.com>
2016-08-23 11:24:51 +0100

    Demo Commit
    Commit

---
+++ DemoSDK/sdk/src/main/java/com/core/wearnext/sdk/WearActionBar.java
@@ -0,0 +1,26 @@
+package com.core.wearnext.sdk;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Fin on 23/08/2016.
+ */
+public class WearActionBar extends View {
+    public WearActionBar(Context context) {
+        super(context);
+    }
+
+    public WearActionBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WearActionBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public WearActionBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
