81eaae5b171636376429334fbbcf2387bf3a799e
lhopkins <lukehopkinsdev@gmail.com>
2016-09-01 18:15:18 +0100

    Basic Styles
    Basic Styles

--- sdk/src/main/java/com/core/wearnext/sdk/WearActionBar.java
+++ sdk/src/main/java/com/core/wearnext/sdk/WearActionBar.java
@@ -1,19 +1,28 @@
 package com.core.wearnext.sdk;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by Fin on 23/08/2016.
  */
-public class WearActionBar extends View {
+public class WearActionBar extends RelativeLayout implements View.OnClickListener {
+    private TextView mMyControls, mShowHelp;
+
     public WearActionBar(Context context) {
         super(context);
+        init();
     }
 
     public WearActionBar(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public WearActionBar(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -22,6 +31,24 @@ public class WearActionBar extends View {
     }
 
     protected void init(){
-        //View.inflate(getContext(), R.layout.gi_button_layout, this);
+        View.inflate(getContext(), R.layout.wear_actionbar, this);
+
+        mShowHelp = (TextView)findViewById(R.id.show_help);
+        mMyControls = (TextView)findViewById(R.id.my_controls);
+        mMyControls.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        if(view.equals(mMyControls)){
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName("com.android.settings","com.android.settings.Settings"));
+            getContext().startActivity(intent);
+        }
+    }
+
+    public void addCommands(View view){
+        addView(view);
+    }
+
 }
---
+++ sdk/src/main/java/com/core/wearnext/sdk/views/WearableListView.java
@@ -0,0 +1,28 @@
+package com.core.wearnext.sdk.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Fin on 31/08/2016.
+ */
+public class WearableListView extends RelativeLayout {
+
+    public WearableListView(Context context) {
+        super(context);
+    }
+
+    public WearableListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WearableListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void init(){
+
+    }
+
+}
