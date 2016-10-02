a59f0fd3612b364631c652ec56a3c662170cd6e5
jackblitz <lukehopkinsdev@gmail.com>
2016-10-02 14:11:36 +0100

    New SpeechComands Array

--- sdk/src/main/java/com/core/realwear/sdk/Util/SpeechCommands.java
+++ sdk/src/main/java/com/core/realwear/sdk/Util/SpeechCommands.java
@@ -3,16 +3,26 @@ package com.core.realwear.sdk.Util;
 import android.content.Context;
 import android.content.Intent;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by Fin on 18/09/2016.
  */
 public class SpeechCommands {
-        public static void UpdateHelp(Context context, String commands){
 
-            Intent intent = new Intent();
-            intent.setAction("com.realware.wearhf.intent.action.UPDATE_HELP_COMMANDS");
-            intent.putExtra("com.realware.wearhf.intent.extra.HELP_COMMANDS", commands);
-            context.sendBroadcast(intent);
-        }
+    @Deprecated
+    public static void UpdateHelp(Context context, String commands) {
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(commands.split(",")));
+        UpdateHelp(context, list);
+    }
+
+    public static void UpdateHelp(Context context, ArrayList<String> commands){
+        Intent intent = new Intent();
+        intent.setAction("com.realware.wearhf.intent.action.UPDATE_HELP_COMMANDS");
+        intent.putStringArrayListExtra("com.realware.wearhf.intent.extra.HELP_COMMANDS", commands);
+        context.sendBroadcast(intent);
+    }
+
 
 }
