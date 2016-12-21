33ab4b959db0e679f0be79bee594306f78a57c97
hnirt <nt@winged.info>
2016-12-21 10:19:08 +0000

    Broadcast intents to com.realwear.wearhf. Added refreshUI().

--- sdk/src/main/java/com/core/realwear/sdk/Util/SpeechCommands.java
+++ sdk/src/main/java/com/core/realwear/sdk/Util/SpeechCommands.java
@@ -10,9 +10,11 @@ import java.util.Arrays;
  * Created by Fin on 18/09/2016.
  */
 public class SpeechCommands {
+    public static final String WEARHF_PACKAGE = "com.realwear.wearhf";
     public static final String ACTION_UPDATE_HELP_COMMANDS = "com.realwear.wearhf.intent.action.UPDATE_HELP_COMMANDS";
     public static final String EXTRA_HELP_COMMANDS = "com.realwear.wearhf.intent.extra.HELP_COMMANDS";
     public static final String EXTRA_SOURCE_PACKAGE = "com.realwear.wearhf.intent.extra.SOURCE_PACKAGE";
+    public static final String ACTION_UPDATE_REFRESH_UI = "com.realwear.wearhf.intent.action.REFRESH_UI";
 
     @Deprecated
     public static void UpdateHelp(Context context, String commands) {
@@ -27,8 +29,17 @@ public class SpeechCommands {
 
     public static void updateHelp(Context context, ArrayList<String> commands) {
         final Intent intent = new Intent();
+        intent.setPackage(WEARHF_PACKAGE);
         intent.setAction(ACTION_UPDATE_HELP_COMMANDS);
         intent.putStringArrayListExtra(EXTRA_HELP_COMMANDS, commands);
+        intent.putExtra(EXTRA_SOURCE_PACKAGE, context.getPackageName());
+        context.sendBroadcast(intent);
+    }
+
+    public static void refreshUI(Context context) {
+        final Intent intent = new Intent();
+        intent.setPackage(WEARHF_PACKAGE);
+        intent.setAction(ACTION_UPDATE_REFRESH_UI);
         intent.putExtra(EXTRA_SOURCE_PACKAGE, context.getPackageName());
         context.sendBroadcast(intent);
     }
